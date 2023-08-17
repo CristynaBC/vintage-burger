187d37f6eaf80c9e479e43643d80597b3306c108
CristynaBC <cristynabcosta@gmail.com>
2023-08-17 11:03:29 -0300

    STYLE: adicionada estrutura inicial da pag Home

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { OptionsComponent } from './options/options.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    OptionsComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/components/home/options/options.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-options',
+  templateUrl: './options.component.html',
+  styleUrls: ['./options.component.css']
+})
+export class OptionsComponent {
+
+}
