8c3a9f5b2a77a7defa6a32b012d81f800eb284a8
geanemr <geaneramos1995@gmail.com>
2023-08-23 11:27:26 -0300

    CHORE: armazenamento do token no order.service.ts para acesso aos arrays de pedidos

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service'; 
 
 @Injectable({
   providedIn: 'root'
@@ -8,13 +9,15 @@ import { Observable } from 'rxjs';
 export class OrderService {
   private apiUrl = 'http://localhost:8080/products';
 
-  constructor(private http: HttpClient) { } //permite usar o HttpClient para fazer chamadas HTTP para a API.
+  constructor(private http: HttpClient, private authService: AuthService) { } //permite usar o HttpClient para fazer chamadas HTTP para a API.
 
   getProductsByType(type: string): Observable<any> { //usa o método get do HttpClient para fazer uma requisição HTTP GET para a URL composta pela base apiUrl concatenada com o tipo de produto selecionado
+    const token = this.authService.getToken(); // Obtenha o token do AuthService
+
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type':  'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImNsYXVkaWFAdmJ1cmdlci5jb20iLCJpYXQiOjE2OTI3Mjc3NzgsImV4cCI6MTY5MjczMTM3OCwic3ViIjoiMSJ9.Cm2siX7Une2RtWpPe47VjOU5blROZVAyquMdjEotex4'
+        Authorization: `Bearer ${token}` // Usa o token obtido do AuthService
       })
     };
     return this.http.get(`${this.apiUrl}`, httpOptions);
