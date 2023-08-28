e78a606a57a9dfcecc39f14715086e7ca86bfa99
CristynaBC <cristynabcosta@gmail.com>
2023-08-28 09:24:01 -0300

    CHORE: adiciona header no new-rders

--- src/app/components/orders/orders.module.ts
+++ src/app/components/orders/orders.module.ts
@@ -5,6 +5,7 @@ import { CommonModule } from '@angular/common';
 import { OrdersRoutingModule } from './orders-routing.module';
 import { NewOrdersComponent } from './new-orders/new-orders.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
+import { CommonsModule } from '../commons/commons.module';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { OrderSummaryComponent } from './order-summary/order-summary.component';
   imports: [
     CommonModule,
     OrdersRoutingModule,
+    CommonsModule
   ],
   exports: [
     OrdersComponent
