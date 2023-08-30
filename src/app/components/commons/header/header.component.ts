29ee2ea22ba852b8307336b3d8edaa738cdccd10
CristynaBC <cristynabcosta@gmail.com>
2023-08-30 09:31:25 -0300

    FEAT: criaçao do menu hamburger

--- src/app/components/commons/header/header.component.ts
+++ src/app/components/commons/header/header.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
