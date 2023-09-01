823cf25b98f5d89ac840e839da896ba3e3be6544
CristynaBC <cristynabcosta@gmail.com>
2023-08-31 22:29:03 -0300

    CHORE: atualização do menu hamburger

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { MatIconModule } from '@angular/material/icon';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatMenuModule } from '@angular/material/menu'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/commons/footer/footer.component';
@@ -25,6 +25,7 @@ import { OrderService } from './services/order.service';
     MatIconModule,
     FormsModule,
     HttpClientModule,
+    MatMenuModule
   ],
   providers: [
     AuthService,
