787bba939cab0337e4f4d9a1e2f5edfa318bbb15
Geane Ramos <geaneramos1995@gmail.com>
2023-09-11 17:43:53 -0300

    TEST: correções na configuração

--- src/app/components/login/form-login/form-login.component.spec.ts
+++ src/app/components/login/form-login/form-login.component.spec.ts
@@ -1,6 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { AuthService } from 'src/app/services/auth/auth.service';
-import { Router } from '@angular/router';
 import { FormLoginComponent } from './form-login.component';
 
 describe('FormLoginComponent', () => {
