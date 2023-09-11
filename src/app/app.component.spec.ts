0dbc8d69d976f7889e4bfdd1374966fcc9a5a78f
Geane Ramos <geaneramos1995@gmail.com>
2023-09-11 17:41:28 -0300

    TEST: início da configuração para testes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-describe('AppComponent', () => {
+fdescribe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
     imports: [RouterTestingModule],
-    declarations: [AppComponent]
+    declarations: [AppComponent],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
   }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'vintage-burger'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('vintage-burger');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('vintage-burger app is running!');
   });
 });
--- src/app/components/login/form-login/form-login.component.spec.ts
+++ src/app/components/login/form-login/form-login.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { Router } from '@angular/router';
 import { FormLoginComponent } from './form-login.component';
 
 describe('FormLoginComponent', () => {
