257bddaa2328863a0026e4966846a6794e163c41
geanemr <geaneramos1995@gmail.com>
2023-09-13 08:43:18 -0300

    CHORE: inclusão do direcionamento para a página de login após fechar o alert e inserção do login-message na página de cardápio

--- src/app/components/login/login-message/login-message.component.ts
+++ src/app/components/login/login-message/login-message.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-message',
@@ -9,7 +10,7 @@ import { AuthService } from 'src/app/services/auth/auth.service';
 export class LoginMessageComponent {
   isModalVisible: boolean = false;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
     // Verifica o estado de login ao inicializar o componente
     this.isModalVisible = !this.authService.isLoggedIn(); // Exibe o modal se o usuário não estiver logado
   }
@@ -22,6 +23,7 @@ export class LoginMessageComponent {
   closeAlert() {
     this.isModalVisible = false; // Fecha o modal
     console.log('Método closeAlert foi chamado');
+    this.router.navigate(['']);
   }
 }
 
