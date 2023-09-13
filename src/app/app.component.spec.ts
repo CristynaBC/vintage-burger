b2360b6df2230d6bbcc21e7da6db418dffb2c326
geanemr <geaneramos1995@gmail.com>
2023-09-13 12:49:30 -0300

    TEST: teste do componente form-login

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,7 +3,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-fdescribe('AppComponent', () => {
+describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
     imports: [RouterTestingModule],
     declarations: [AppComponent],
--- src/app/components/login/form-login/form-login.component.spec.ts
+++ src/app/components/login/form-login/form-login.component.spec.ts
@@ -1,13 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormLoginComponent } from './form-login.component';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { Router } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
-describe('FormLoginComponent', () => {
+fdescribe('FormLoginComponent', () => {
   let component: FormLoginComponent;
   let fixture: ComponentFixture<FormLoginComponent>;
+  let fakeAuthService: AuthService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [FormLoginComponent]
+  beforeEach(async () => {
+    fakeAuthService = jasmine.createSpyObj<AuthService>(
+      'AuthService',
+      {
+        login:  of (Observable<any>),
+        getToken: "test",
+        isLoggedIn: true,
+        getRole: "test",
+        
+      }
+    );
+    await TestBed.configureTestingModule({
+      declarations: [FormLoginComponent],
+      imports:[FormsModule],
+      providers:[
+        { provide: AuthService, useValue: fakeAuthService }, Router]
     });
     fixture = TestBed.createComponent(FormLoginComponent);
     component = fixture.componentInstance;
