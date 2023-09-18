86e384b2809540e83231ecbb486454acdefa1bd6
geanemr <geaneramos1995@gmail.com>
2023-09-18 17:11:38 -0300

    TEST: testes admin.component

--- src/app/components/options/admin/admin.component.spec.ts
+++ src/app/components/options/admin/admin.component.spec.ts
@@ -1,14 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AdminComponent } from './admin.component';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { Router } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { FormsModule } from '@angular/forms';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-describe('AdminComponent', () => {
+fdescribe('AdminComponent', () => {
   let component: AdminComponent;
   let fixture: ComponentFixture<AdminComponent>;
+  let fakeAuthService: AuthService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [AdminComponent]
+  beforeEach(async () => {
+    fakeAuthService = jasmine.createSpyObj<AuthService>(
+      'AuthService',
+      {
+        login:  of (Observable<any>),
+        getToken: "test",
+        isLoggedIn: true,
+        getRole: "test",
+        
+      }
+    );
+    await TestBed.configureTestingModule({
+      declarations: [AdminComponent],
+      imports:[FormsModule],
+      schemas:[CUSTOM_ELEMENTS_SCHEMA],
+      providers:[
+        { provide: AuthService, useValue: fakeAuthService }, Router]
     });
     fixture = TestBed.createComponent(AdminComponent);
     component = fixture.componentInstance;
