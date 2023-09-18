d9bd619bd5d06f81e5a836afc3fc03c3f4dc0361
geanemr <geaneramos1995@gmail.com>
2023-09-18 16:47:41 -0300

    teste orders.component

--- src/app/components/orders/orders.component.spec.ts
+++ src/app/components/orders/orders.component.spec.ts
@@ -1,14 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { OrdersComponent } from './orders.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { Observable,of } from 'rxjs';
+import { Router } from '@angular/router';
 
-describe('OrdersComponent', () => {
+
+fdescribe('OrdersComponent', () => {
   let component: OrdersComponent;
   let fixture: ComponentFixture<OrdersComponent>;
+  let fakeAuthService: AuthService;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    fakeAuthService = jasmine.createSpyObj<AuthService>(
+      'AuthService',
+      {
+        login:  of (Observable<any>),
+        getToken: "test",
+        isLoggedIn: true,
+        getRole: "test",
+        
+      }
+    );
     TestBed.configureTestingModule({
-      declarations: [OrdersComponent]
+      imports:[RouterTestingModule],
+      declarations: [OrdersComponent],
+      schemas:[CUSTOM_ELEMENTS_SCHEMA],
+      providers:[
+        { provide: AuthService, useValue: fakeAuthService }, Router]
     });
     fixture = TestBed.createComponent(OrdersComponent);
     component = fixture.componentInstance;
