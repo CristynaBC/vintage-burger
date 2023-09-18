52abef149a11130bc08f2608cf4697726291c0e2
geanemr <geaneramos1995@gmail.com>
2023-09-18 16:35:58 -0300

    TEST: teste options.component

--- src/app/components/options/options.component.spec.ts
+++ src/app/components/options/options.component.spec.ts
@@ -1,14 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { OptionsComponent } from './options.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { Observable,of } from 'rxjs';
+import { Router } from '@angular/router';
+
 
 describe('OptionsComponent', () => {
   let component: OptionsComponent;
   let fixture: ComponentFixture<OptionsComponent>;
+  let fakeAuthService: AuthService;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    fakeAuthService = jasmine.createSpyObj<AuthService>(
+      'AuthService',
+      {
+        login:  of (Observable<any>),
+        getToken: "test",
+        isLoggedIn: true,
+        getRole: "test",
+        
+      }
+    );
     TestBed.configureTestingModule({
-      declarations: [OptionsComponent]
+      imports:[RouterTestingModule],
+      declarations: [OptionsComponent],
+      schemas:[CUSTOM_ELEMENTS_SCHEMA],
+      providers:[
+        { provide: AuthService, useValue: fakeAuthService }, Router]
     });
     fixture = TestBed.createComponent(OptionsComponent);
     component = fixture.componentInstance;
