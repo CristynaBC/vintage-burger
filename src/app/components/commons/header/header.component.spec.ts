7f5c6d5d015363f28ef0acaf07e790227df34240
geanemr <geaneramos1995@gmail.com>
2023-09-19 17:12:31 -0300

    TEST: teste do header.component

--- src/app/components/commons/header/header.component.spec.ts
+++ src/app/components/commons/header/header.component.spec.ts
@@ -1,15 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { HeaderComponent } from './header.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { Observable,of } from 'rxjs';
+import { Router } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let fakeAuthService: AuthService;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    fakeAuthService = jasmine.createSpyObj<AuthService>(
+      'AuthService',
+      {
+        login:  of (Observable<any>),
+        getToken: "test",
+        isLoggedIn: true,
+        getRole: "test",
+        
+      }
+    );
     TestBed.configureTestingModule({
-      declarations: [HeaderComponent]
-    });
+      imports:[RouterTestingModule,MatMenuModule],
+      declarations: [HeaderComponent],
+      schemas:[CUSTOM_ELEMENTS_SCHEMA],
+      providers:[
+        { provide: AuthService, useValue: fakeAuthService }, Router]
+    }),
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
