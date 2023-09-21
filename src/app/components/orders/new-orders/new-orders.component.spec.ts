902ba47f531e14fe5e519eaa9333ffe99d0b7f2f
CristynaBC <cristynabcosta@gmail.com>
2023-09-20 23:39:28 -0300

    TEST: teste para a criação do componente new-orders

--- src/app/components/orders/new-orders/new-orders.component.spec.ts
+++ src/app/components/orders/new-orders/new-orders.component.spec.ts
@@ -1,14 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ProductsService } from 'src/app/services/products/products.service';
+import { OrderService } from 'src/app/services/order/order.service';
 import { NewOrdersComponent } from './new-orders.component';
+import { Observable, of } from 'rxjs';
+
 
 describe('NewOrdersComponent', () => {
   let component: NewOrdersComponent;
   let fixture: ComponentFixture<NewOrdersComponent>;
+  let fakeProductsService: ProductsService;
+  let fakeOrderService: OrderService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [NewOrdersComponent]
+  beforeEach(async () => {
+    (fakeProductsService = jasmine.createSpyObj<ProductsService>(
+      'ProductsService',
+      {
+        getProducts: of([]),
+      }
+    )),
+      (fakeOrderService = jasmine.createSpyObj('OrderService', ['addToCart']));
+
+    await TestBed.configureTestingModule({
+      declarations: [NewOrdersComponent],
+      providers: [
+        { provide: ProductsService, useValue: fakeProductsService },
+        { provide: OrderService, useValue: fakeOrderService }],
     });
     fixture = TestBed.createComponent(NewOrdersComponent);
     component = fixture.componentInstance;
