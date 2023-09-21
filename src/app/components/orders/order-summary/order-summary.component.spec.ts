40abadc8f4a733945aca79e6924e584ae105736b
CristynaBC <cristynabcosta@gmail.com>
2023-09-20 23:57:39 -0300

    TEST: teste para a criação do componente order-summary

--- src/app/components/orders/order-summary/order-summary.component.spec.ts
+++ src/app/components/orders/order-summary/order-summary.component.spec.ts
@@ -1,14 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { OrderService } from 'src/app/services/order/order.service';
 import { OrderSummaryComponent } from './order-summary.component';
+import { FormsModule } from '@angular/forms';
 
 describe('OrderSummaryComponent', () => {
   let component: OrderSummaryComponent;
   let fixture: ComponentFixture<OrderSummaryComponent>;
+  let fakeOrderService: OrderService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [OrderSummaryComponent]
+  beforeEach(async () => {
+
+    (fakeOrderService = jasmine.createSpyObj('OrderService', [
+      'calculateTotal',
+      'increaseQuantity',
+      'decreaseQuantity',
+      'removeItem',
+      'saveCartItems',
+      'clearCart',
+      'getCartItems'
+    ]));
+
+    await TestBed.configureTestingModule({
+      declarations: [OrderSummaryComponent],
+      imports: [FormsModule],
+      providers: [{ provide: OrderService, useValue: fakeOrderService }],
     });
     fixture = TestBed.createComponent(OrderSummaryComponent);
     component = fixture.componentInstance;
