8a731a9f2ac0df0021a63488309bbefa0d6029da
geanemr <geaneramos1995@gmail.com>
2023-09-22 16:23:02 -0300

    TEST: testes employees e products.component

--- src/app/components/edit/employees/employees.component.spec.ts
+++ src/app/components/edit/employees/employees.component.spec.ts
@@ -1,21 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { EmployeesComponent } from './employees.component';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { Router } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 describe('EmployeesComponent', () => {
   let component: EmployeesComponent;
   let fixture: ComponentFixture<EmployeesComponent>;
+  let fakeAuthService: AuthService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [EmployeesComponent]
+  beforeEach(async () => {
+    fakeAuthService = jasmine.createSpyObj<AuthService>(
+      'AuthService',
+      {
+        login:  of (Observable<any>),
+        getToken: "test",
+        isLoggedIn: true,
+        getRole: "test",
+        
+      }
+    );
+    await TestBed.configureTestingModule({
+      declarations: [EmployeesComponent],
+      schemas:[CUSTOM_ELEMENTS_SCHEMA],
+      providers:[
+        { provide: AuthService, useValue: fakeAuthService }, Router, HttpClient, HttpHandler]
     });
     fixture = TestBed.createComponent(EmployeesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
 });
