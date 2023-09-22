0636bea510377fb01370d9a3729f00726d2760e5
geanemr <geaneramos1995@gmail.com>
2023-09-22 16:50:51 -0300

    TEST: inclusão dos providers para passar no primeiro teste dos serviços de order e products

--- src/app/services/employees/employees.service.spec.ts
+++ src/app/services/employees/employees.service.spec.ts
@@ -1,16 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
 import { EmployeesService } from './employees.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 describe('EmployeesService', () => {
   let service: EmployeesService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [HttpClient, HttpHandler]
+    });
     service = TestBed.inject(EmployeesService);
   });
 
-  it('should be created', () => {
+  it('should create the service', () => {
     expect(service).toBeTruthy();
   });
 });
--- src/app/services/products/products.service.spec.ts
+++ src/app/services/products/products.service.spec.ts
@@ -1,16 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ProductsService } from './products.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 describe('ProductsService', () => {
   let service: ProductsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [HttpClient, HttpHandler]
+    });
     service = TestBed.inject(ProductsService);
   });
 
-  it('should be created', () => {
+  it('should create the service', () => {
     expect(service).toBeTruthy();
   });
 });
